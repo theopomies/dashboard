77f6c46ce2a0974403c3fa94ccda83ec91b172e8
Théo Pomies <theo.pomies@outlook.com>
2021-11-30 15:54:01 +0100

    chore: fix styles for calendar

--- dashboard/src/components/Widgets/GithubCalendar.tsx
+++ dashboard/src/components/Widgets/GithubCalendar.tsx
@@ -3,8 +3,13 @@ import GitHubCalendar from "react-github-calendar";
 
 export function GithubCalendar() {
   return (
-    <Box padding="1rem" paddingTop="3rem" color="brand.darkGray">
-      <GitHubCalendar username="theopomies" hideColorLegend />
+    <Box
+      padding="1rem"
+      paddingTop="3rem"
+      color="brand.darkGray"
+      fontWeight="500"
+    >
+      <GitHubCalendar username="theopomies" hideColorLegend fontSize={16} />
     </Box>
   );
 }
